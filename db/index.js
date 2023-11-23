1b616be554d506afbeecbbf85d1a4a43ff2fc081
LAPTOP-T6D3E1EO\Maples <1764070917@qq.com>
2023-11-24 01:12:33 +0800

    更改数据库为qzh

--- db/index.js
+++ db/index.js
@@ -5,9 +5,9 @@ const mysql = require('mysql')
 // 创建与数据库的连接
 const db = mysql.createPool({
 	host:'localhost',
-	user:'admin',
+	user:'qzh',
 	password:'123456',
-	database:'admin'
+	database:'qzh'
 })
 
 // 对外暴露数据库
